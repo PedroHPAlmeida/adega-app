1b9a6b3fdd6f959ca5499aee590e780fd5e2337e
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-09 20:13:36 -0300

    first commit

---
+++ backend/src/test/java/br/com/adega/apiadega/ApiadegaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adega.apiadega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiadegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
